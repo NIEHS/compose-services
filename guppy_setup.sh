1ed1ce95e57d0b6f55d4fefc8548384087d4b962
deeppatelcs <deeppatel.cs@gmail.com>
2023-03-13 16:42:19 -0400

    update guppy_setup.sh index

--- guppy_setup.sh
+++ guppy_setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Script to create and re-create es indices and setup guppy
 sleep 2
+docker exec esproxy-service curl -X DELETE http://localhost:9200/population_data_resource-array-config_0
+sleep 2
+docker exec esproxy-service curl -X DELETE http://localhost:9200/population_data_resource_0
+sleep 2
 docker exec esproxy-service curl -X DELETE http://localhost:9200/geospatial_data_resource_0
 sleep 2
 docker exec esproxy-service curl -X DELETE http://localhost:9200/study_file_0
